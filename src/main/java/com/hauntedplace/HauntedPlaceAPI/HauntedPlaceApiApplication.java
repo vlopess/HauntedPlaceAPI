299c9995a3472c099439767e1cd7a63d3fa69865
victor <victorlopes9291@gmail.com>
2024-04-06 11:07:15 -0300

    Init project

---
+++ src/main/java/com/hauntedplace/HauntedPlaceAPI/HauntedPlaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.hauntedplace.HauntedPlaceAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HauntedPlaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HauntedPlaceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hauntedplace/HauntedPlaceAPI/HauntedPlaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hauntedplace.HauntedPlaceAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HauntedPlaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
