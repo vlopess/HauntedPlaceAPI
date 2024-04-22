461ea88b80471ab0ca9a986390f0b4650f5c53cf
victor <victorlopes9291@gmail.com>
2024-04-21 22:09:47 -0300

    Criação da representação de SocialMedia (Entity)

---
+++ src/main/java/com/hauntedplace/HauntedPlaceAPI/Entitys/SocialMedia.java
@@ -0,0 +1,41 @@
+package com.hauntedplace.HauntedPlaceAPI.Entitys;
+
+import com.hauntedplace.HauntedPlaceAPI.Models.Enums.TagEnum;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "social_medias")
+public class SocialMedia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public SocialMedia() {}
+
+    public SocialMedia(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+    public SocialMedia(Long id) {
+        this.id = id;
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
