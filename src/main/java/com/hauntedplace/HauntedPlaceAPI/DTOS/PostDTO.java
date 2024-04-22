9e45585e8c67a5f988829c544594f6b86734c144
victor <victorlopes9291@gmail.com>
2024-04-21 22:21:42 -0300

    Ajustes nos atributos de PostDTO

--- src/main/java/com/hauntedplace/HauntedPlaceAPI/DTOS/PostDTO.java
+++ src/main/java/com/hauntedplace/HauntedPlaceAPI/DTOS/PostDTO.java
@@ -2,18 +2,20 @@ package com.hauntedplace.HauntedPlaceAPI.DTOS;
 
 import com.hauntedplace.HauntedPlaceAPI.Entitys.Post;
 import com.hauntedplace.HauntedPlaceAPI.Entitys.User;
+import com.hauntedplace.HauntedPlaceAPI.Models.EncryptedId;
 import com.hauntedplace.HauntedPlaceAPI.Models.Enums.TagEnum;
+import com.hauntedplace.HauntedPlaceAPI.Models.UserOverView;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.Date;
 
 public class PostDTO {
-    private Long id;
+    private String encryptedId;
     @NotBlank(message = "title is required")
     private String title;
     private String urlImage;
-    private UserDTO user;
+    private UserOverView user;
     private TagEnum tag;
     @NotBlank(message = "content is required")
     private String content;
@@ -21,8 +23,8 @@ public class PostDTO {
 
     public PostDTO() {}
 
-    public PostDTO(Long id, String title, String urlImage, UserDTO user, TagEnum tag, String content, Date createdAt) {
-        this.id = id;
+    public PostDTO(String encryptedId, String title, String urlImage, UserOverView user, TagEnum tag, String content, Date createdAt) {
+        this.encryptedId = encryptedId;
         this.title = title;
         this.urlImage = urlImage;
         this.user = user;
@@ -32,21 +34,21 @@ public class PostDTO {
     }
 
     public PostDTO(Post post){
-        this.id = post.getId();
+        this.encryptedId = new EncryptedId(post.getId()).get();
         this.title = post.getTitle();
         this.urlImage = post.getUrlImage();
-        this.user = new UserDTO(post.getUser());
+        this.user = new UserOverView(post.getUser());
         this.content = post.getContent();
         this.createdAt = post.getCreatedAt();
         this.tag = TagEnum.valueOf(post.getTag().getDescription());
     }
 
-    public Long getId() {
-        return id;
-    }
+//    public Long getId() {
+//        return new EncryptedId(encryptedId).getDecrypted();
+//    }
 
-    public void setId(Long id) {
-        this.id = id;
+    public String getEncryptedId() {
+        return encryptedId;
     }
 
     public String getTitle() {
@@ -63,11 +65,11 @@ public class PostDTO {
     public void setUrlImage(String urlImage) {
         this.urlImage = urlImage;
     }
-    public UserDTO getUser() {
+    public UserOverView getUser() {
         return user;
     }
 
-    public void setUser(UserDTO user) {
+    public void setUser(UserOverView user) {
         this.user = user;
     }
     public TagEnum getTag() {
