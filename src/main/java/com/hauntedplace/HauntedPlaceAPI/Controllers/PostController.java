a84f1ce7487049278ff0a2b526c14d924111785d
victor <victorlopes9291@gmail.com>
2024-04-21 22:23:05 -0300

    Usando o try/catch na Controller de Post

--- src/main/java/com/hauntedplace/HauntedPlaceAPI/Controllers/PostController.java
+++ src/main/java/com/hauntedplace/HauntedPlaceAPI/Controllers/PostController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -28,28 +29,49 @@ public class PostController {
 
     @PostMapping("/publicar")
     public ResponseEntity<String> postar(@RequestBody @Valid PostDTO postDTO, UriComponentsBuilder uriBuilder){
-        var post = postService.save(postDTO);
-        URI uri = uriBuilder.path("/post/{id}").buildAndExpand(postDTO.getId()).toUri();
-        return ResponseEntity.created(uri).body("Post created!");
+        try {
+            var post = postService.save(postDTO);
+            URI uri = uriBuilder.path("/post/{id}").buildAndExpand(post.getEncryptedId()).toUri();
+            return ResponseEntity.created(uri).body("Post created!");
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PostDTO> getPostById(@PathVariable Long id){
-        return postService.getPostById(id);
+    public ResponseEntity<Object> getPostById(@PathVariable Long id){
+        try{
+            return postService.getPostById(id);
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
     }
 
     @GetMapping("/user/{username}")
-    public ResponseEntity<List<PostDTO>> getPostById(@PathVariable String username){
-        return postService.getPostAllUser(username);
+    public ResponseEntity<List<Object>> getPostById(@PathVariable String username){
+        try{
+            return postService.getAllUserPosts(username);
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(Collections.singletonList(e.getMessage()));
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<PostDTO> updatePost(@PathVariable Long id, @RequestBody PostDTO postDTO){
-        return postService.update(id, postDTO);
+    public ResponseEntity<Object> updatePost(@PathVariable Long id, @RequestBody PostDTO postDTO){
+        try{
+            return postService.update(id, postDTO);
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deletePost(@PathVariable Long id){
-        return postService.deletePost(id);
+        try{
+            return postService.deletePost(id);
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
     }
 }
