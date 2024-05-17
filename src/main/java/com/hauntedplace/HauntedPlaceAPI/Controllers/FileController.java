ed5174c6c7863e01da70cdf4cdc1654fc9d7e26b
victor <victorlopes9291@gmail.com>
2024-05-17 20:17:40 -0300

    Criação da controller para manipulação de arquivos

---
+++ src/main/java/com/hauntedplace/HauntedPlaceAPI/Controllers/FileController.java
@@ -0,0 +1,37 @@
+package com.hauntedplace.HauntedPlaceAPI.Controllers;
+
+import com.hauntedplace.HauntedPlaceAPI.Models.StringWrapper;
+import com.hauntedplace.HauntedPlaceAPI.Services.FirebaseStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/files")
+public class FileController {
+
+    private final FirebaseStorageService firebaseStorageService;
+
+    @Autowired
+    public FileController(FirebaseStorageService firebaseStorageService) {
+        this.firebaseStorageService = firebaseStorageService;
+    }
+
+
+    @PostMapping("/files/upload")
+    public ResponseEntity<Object> uploadFileUser(@RequestParam("file") MultipartFile file){
+        StringWrapper filePath = firebaseStorageService.upload(file);
+        return ResponseEntity.ok().body(filePath);
+    }
+
+    @PostMapping("/files/remove/{filename}")
+    public ResponseEntity<Object> removeFileUser(@PathVariable String filename) throws Exception {
+        StringWrapper filePath = firebaseStorageService.remove(filename);
+        return ResponseEntity.ok().body(filePath);
+    }
+}
