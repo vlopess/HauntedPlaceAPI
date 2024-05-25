d7c6ecb98d31b4cf07cadb60e644bac2de53f750
victor <victorlopes9291@gmail.com>
2024-05-25 18:38:34 -0300

    refact: mudando o retorno dos métodos para Optional

--- src/main/java/com/hauntedplace/HauntedPlaceAPI/Repository/UserRepository.java
+++ src/main/java/com/hauntedplace/HauntedPlaceAPI/Repository/UserRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    UserDetails findByUsername(String username);
-    UserDetails findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
