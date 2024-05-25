d917e51371c741ca2ea3bb53f13c99214480404b
victor <victorlopes9291@gmail.com>
2024-05-25 18:35:03 -0300

    refact: ajustes no JWTAuthorizationFilter

--- src/main/java/com/hauntedplace/HauntedPlaceAPI/Security/filters/JWTAuthorizationFilter.java
+++ src/main/java/com/hauntedplace/HauntedPlaceAPI/Security/filters/JWTAuthorizationFilter.java
@@ -1,49 +1,52 @@
-package com.hauntedplace.HauntedPlaceAPI.config;
+package com.hauntedplace.HauntedPlaceAPI.Security.filters;
 
-import com.hauntedplace.HauntedPlaceAPI.Repository.UserRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.hauntedplace.HauntedPlaceAPI.Services.JWTokenService;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 @WebFilter(urlPatterns = "/api/**")
-public class SecurityFilter extends OncePerRequestFilter {
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
-    private final JWTokenService jwTokenService;
-    private final UserRepository userRepository;
+
+    private final JWTokenService jwtTokenService;
 
     @Autowired
-    public SecurityFilter(JWTokenService jwTokenService, UserRepository userRepository) {
-        this.jwTokenService = jwTokenService;
-        this.userRepository = userRepository;
+    public JWTAuthorizationFilter(JWTokenService jwtTokenService) {
+        this.jwtTokenService = jwtTokenService;
     }
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = getToken(request);
         if (tokenJWT != null) {
-            var username = jwTokenService.verifyToken(tokenJWT);
-            var user = userRepository.findByUsername(username);
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            var username = jwtTokenService.verifyToken(tokenJWT);
+            var authentication = new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
         filterChain.doFilter(request,response);
     }
 
-    private String getToken(HttpServletRequest request) {
+    public String getToken(HttpServletRequest request) {
         var token = request.getHeader("Authorization");
         if (token == null || !token.startsWith("Bearer ")) {
             return null;
         }
         return token.replace("Bearer ", "");
     }
+
 }
